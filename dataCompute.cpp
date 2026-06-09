acacd5c1fec9f041355b7a524d6a32519d94eafa
idgg007 <idgg007@163.com>
2026-06-09 11:46:16 +0800

    停止工作，甲方改需求了
    Signed-off-by: idgg007 <idgg007@163.com>

--- dataCompute.cpp
+++ dataCompute.cpp
@@ -16,7 +16,7 @@ struct Buff
 vector<string> tag;
 vector<string> ruleFrom;
 vector<std::vector<Buff>> buff;
-vector<std::vector<double>> devide;
+vector<std::vector<double>> chance;
 vector<vector<pair<int, int>>> man;
 vector<int> gate;
 vector<int> sum_1;
@@ -32,7 +32,7 @@ void init()
     sum_1.assign(count_country, 0);
     sum_2.assign(count_country, 0);
     ruleFrom.assign(count_country, "rule.ini");
-    devide.resize(count_country);
+    chance.resize(count_country);
     buff.assign(count_country, vector<Buff>());
     gate.assign(count_country, 5);
     for (int i = 0; i < count_country; i++)
@@ -116,16 +116,12 @@ void init()
     for (int i = 0; i < count_country; i++)
     {
         ifstream readRule(ruleFrom[i]);
-        devide[i].assign(5, 0.0);
+        chance[i].assign(5, 0.0);
         for (int j = 0; j < 5; j++)
         {
-            readRule >> devide[i][j];
+            readRule >> chance[i][j];
         }
     }
-}
-
-double JudgeRule()
-{
 }
 
 vector<double> allDivide;
@@ -134,15 +130,80 @@ set<double> all;
 
 double divide[5];
 
-double ans = 0;
+double ans = 1.0;
 
-int count = 0; // ans更新的次数
+int ansCount = 0; // ans更新的次数
 
-void DFS(const int &count) // 创建枚举比例
+double Judge(const int &d, const int &f, const int &fAdd, const int &dAdd, const int &c)
+{
+    double f = sum_1[f] + fAdd;
+    int tryCount = 1;
+    for (size_t i = 0; i < buff[f].size(); i++)
+    {
+        if ((buff[f][i].type & c) && buff[f][i].l <= fAdd && buff[f][i].r >= fAdd)
+        {
+            tryCount = max(tryCount,buff[f][i].tryCount);
+            f += buff[f][i].add;
+        }
+        
+    }
+}
+
+double JudgeRule()
+{
+    for (size_t i = 0; i < tag.size(); i++)
+    {
+        for (size_t j = i + 1; j < tag.size(); j++)
+        {
+            //
+            double expected_i = 0; // i防守i的收益
+            double expected_j = 0; // j防守j的收益
+            unsigned int limit_i = (1 << (man[i].size()));
+            unsigned int limit_j = (1 << (man[j].size()));
+            for (unsigned int k = 0; k < limit_i; k++)
+            {
+                double newAns = 0;
+                int secondAdd = 0;
+                int firstAdd = 0;
+                for (unsigned int l = 0, c = 1; l < man.size(); l++, (c <<= 1))
+                {
+                    if (c & k)
+                    {
+                        firstAdd += man[i][l].first;
+                    }
+                    else
+                    {
+                        secondAdd += man[i][l].second;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void DFS(const int &count, const size_t &it) // 创建枚举比例
 {
     if (count == 5)
     {
-        // 将divide出来
+        double newAns = JudgeRule();
+        if (newAns < ans)
+        {
+            ansCount++;
+            ans = newAns;
+            char name[20];
+            sprintf(name, "ans%d.txt", ansCount);
+            ofstream writeAns(name);
+            for (int i = 0; i < 5; i++)
+            {
+                writeAns << divide[i] << "\n";
+            }
+            writeAns.close();
+        }
+    }
+    for (size_t i = it + 1; i < allDivide.size(); i++)
+    {
+        divide[count] = allDivide[i];
+        DFS(count + 1, i);
     }
 }
 
@@ -168,4 +229,5 @@ int main()
     }
     sort(allDivide.begin(), allDivide.end());
     init();
+    DFS(0, 0);
 }
